5669da32c95050f526e61c731beed2c16c75a648
justinkim2015 <justinkim2015@gmail.com>
2022-07-21 10:45:51 +0900

    Create table posts

--- micro-reddit/app/models/user.rb
+++ micro-reddit/app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  validates :user, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :email, presence: true, uniqueness: true, format: { with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i,
+                                                                message: "Please use a valid email address" }
 end
--- micro-reddit/db/schema.rb
+++ micro-reddit/db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_21_012245) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_21_014433) do
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "user"
     t.datetime "created_at", null: false
